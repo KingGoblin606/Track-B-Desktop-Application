6817a5618dd3a0f2ec1febdfb603da254eab9c3c
f-jooste <fourie.j789@gmail.com>
2026-07-12 19:30:55 +0200

    ADD: Material table

--- Track B – Desktop Application/src/track/b/desktop/application/Model/DBInitializer.java
+++ Track B – Desktop Application/src/track/b/desktop/application/Model/DBInitializer.java
@@ -29,10 +29,39 @@ public class DBInitializer {
                            UserPassword VARCHAR(255) NOT NULL,
                            UserEmail VARCHAR(100) NOT NULL UNIQUE,
                            UserRole VARCHAR(10) NOT NULL
-                                CHECK (UserRole IN ('User', 'Admin'))
+                           CHECK (UserRole IN ('User', 'Admin'))
                            )
                            """);
     }
     
+    private static void createSuppliersTable(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate("""
+                           CREATE TABLE Supplier (
+                           SupplierID int PRIMARY KEY,
+                           SupplierName VARCHAR(50) NOT NULL,
+                           ContactPerson VARCHAR(50) NOT NULL,
+                           PhoneNumber VARCHAR(20) NOT NULL,
+                           SupplierEmail VARCHAR(100) NOT NULL UNIQUE,
+                           StreetAddress VARCHAR(100) NOT NULL,
+                           City VARCHAR(50) NOT NULL,
+                           PostalCode VARCHAR(10) NOT NULL
+                           )
+                           """);
+    }
     
+    private static void createMaterialTable(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate("""
+                           CREATE TABLE Materials (
+                           MaterialsID INT PRIMARY KEY,
+                           FOREIGN KRY (SupplierID) REFERENCES Supplier(SupplierID),
+                           MaterialName VARCHAR(50) NOT NULL,
+                           Quantity INT NOT NULL,
+                           ReorderLevel INT NOT NULL,
+                           Cost INT NOT NULL
+                           """);
+    }
 }
