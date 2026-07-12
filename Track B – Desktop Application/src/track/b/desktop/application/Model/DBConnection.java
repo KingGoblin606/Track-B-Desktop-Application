8c11ab605eff5ae867315c85b07ee300575a7657
f-jooste <fourie.j789@gmail.com>
2026-07-12 12:31:38 +0200

    Fixed DB connection

--- Track B – Desktop Application/src/track/b/desktop/application/Model/DBConnection.java
+++ Track B – Desktop Application/src/track/b/desktop/application/Model/DBConnection.java
@@ -1,7 +1,25 @@
 package track.b.desktop.application.Model;
-import java.sql.*;
 
-public class DBConnection implement AutoCloseable{
-private static final String JDBC_URL = "jdbc:derby:resouceDB; create=true";
-private Connection con;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection implements AutoCloseable {
+    private static final String JDBC_URL = "jdbc:derby:ResourceManagementDB;create=true";
+    private Connection con;
+    
+    public DBConnection() throws SQLException {
+        con = DriverManager.getConnection(JDBC_URL);
+    }
+    
+    public Connection getConnection() {
+        return con;
+    }
+    
+    @Override
+    public void close() throws SQLException {
+        if (con != null && !con.isClosed()) {
+            con.close();
+        }
+    }
 }
