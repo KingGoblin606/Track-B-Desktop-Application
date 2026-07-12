71bb429e91d4272385d8a1fe8dc5e81c34ae1d5d
f-jooste <fourie.j789@gmail.com>
2026-07-12 12:47:52 +0200

    ADD: comments to DBCOnnection

--- Track B – Desktop Application/src/track/b/desktop/application/Model/DBConnection.java
+++ Track B – Desktop Application/src/track/b/desktop/application/Model/DBConnection.java
@@ -4,20 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection implements AutoCloseable {
+public class DBConnection implements AutoCloseable {//handles db connection
+
+    //connection string
     private static final String JDBC_URL = "jdbc:derby:ResourceManagementDB;create=true";
     private Connection con;
     
-    public DBConnection() throws SQLException {
+    public DBConnection() throws SQLException {//opens connection
         con = DriverManager.getConnection(JDBC_URL);
     }
     
-    public Connection getConnection() {
+    public Connection getConnection() {//reurns active db connection
         return con;
     }
     
     @Override
-    public void close() throws SQLException {
+    public void close() throws SQLException {//automatically closes connection
         if (con != null && !con.isClosed()) {
             con.close();
         }
